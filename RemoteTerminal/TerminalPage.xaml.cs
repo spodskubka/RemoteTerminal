d44d45b22754be2fcb0d3e2ce006e4c3157d535b
Stefan Podskubka <stefan.podskubka@live.at>
2013-04-13 13:44:09 +0200

    Added copy mode to the terminal view

--- RemoteTerminal/TerminalPage.xaml.cs
+++ RemoteTerminal/TerminalPage.xaml.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using RemoteTerminal.Connections;
 using RemoteTerminal.Model;
+using RemoteTerminal.Screens;
 using RemoteTerminal.Terminals;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Core;
+using Windows.UI.Text;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -18,6 +23,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Documents;
+using System.Threading;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
@@ -28,6 +35,7 @@ namespace RemoteTerminal
     /// </summary>
     public sealed partial class TerminalPage : RemoteTerminal.Common.LayoutAwarePage
     {
+        private ManualResetEventSlim screenDisplayCopyBoxLoaded = new ManualResetEventSlim();
         private InputPaneHelper inputPaneHelper;
 
         public static readonly DependencyProperty TerminalProperty = DependencyProperty.Register("Terminal", typeof(ITerminal), typeof(TerminalPage), null);
@@ -55,6 +63,8 @@ namespace RemoteTerminal
             this.inputPaneHelper.SubscribeToKeyboard(true);
             this.inputPaneHelper.AddShowingHandler(this.screenDisplay, new InputPaneShowingHandler(CustomKeyboardHandler));
             this.inputPaneHelper.SetHidingHandler(new InputPaneHidingHandler(InputPaneHiding));
+
+            this.copyContainer.Visibility = Visibility.Collapsed;
         }
 
         /// <summary>
@@ -216,12 +226,205 @@ namespace RemoteTerminal
             this.BottomAppBar.IsOpen = false;
         }
 
+        private async void copyModeAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.copyContainer.Visibility == Visibility.Visible)
+            {
+                this.HideCopyMode();
+                return;
+            }
+
+            await this.ShowCopyMode();
+        }
+
+        private void HideCopyMode()
+        {
+            if (this.copyContainer.Visibility == Visibility.Visible)
+            {
+                this.screenDisplayCopyBox.IsReadOnly = false;
+                this.screenDisplayCopyBox.Document.SetText(TextSetOptions.None, string.Empty);
+                this.copyContainer.Visibility = Visibility.Collapsed;
+                this.BottomAppBar.IsOpen = false;
+                this.TopAppBar.IsOpen = false;
+                this.screenDisplay.Focus(FocusState.Programmatic);
+            }
+        }
+
+        private async Task ShowCopyMode()
+        {
+            this.screenDisplayCopyBox.IsReadOnly = false;
+            this.screenDisplayCopyBox.Background = new SolidColorBrush(Windows.UI.Colors.White);
+            this.screenDisplayCopyBox.Foreground = new SolidColorBrush(Windows.UI.Colors.Black);
+            this.screenDisplayCopyBox.BorderBrush = new SolidColorBrush(Windows.UI.Colors.ForestGreen);
+
+            // We have to wait until the RichEditBox is loaded before we try to load any content into it.
+            // If we don't do that the fonts and colors get lost.
+            this.copyContainer.Visibility = Visibility.Visible;
+            ManualResetEventSlim loadedEvent = this.screenDisplayCopyBoxLoaded;
+            if (loadedEvent != null)
+            {
+                await Task.Run(() => loadedEvent.Wait());
+                loadedEvent.Dispose();
+                this.screenDisplayCopyBoxLoaded = null;
+            }
+
+            var screenCopy = this.Terminal.RenderableScreen.GetScreenCopy();
+
+            // First we generate an RTF document that has every cell set to X.
+            // The reason for this is that we need to determine the final size of the RichEditBox
+            // to resize it correctly. But if the text contains spaces somewhere the ExtentWidth/Height
+            // of the ScrollViewer, which is used to resize the RichEditBox, is off.
+            using (InMemoryRandomAccessStream rtfStream = await this.GenerateRtf(screenCopy, fake: true))
+            {
+                this.screenDisplayCopyBox.Document.LoadFromStream(TextSetOptions.ApplyRtfDocumentDefaults | TextSetOptions.FormatRtf | TextSetOptions.Unhide, rtfStream);
+            }
+
+            this.screenDisplayCopyBoxScroll.Width = this.screenDisplayCopyBoxScroll.ExtentWidth;
+            this.screenDisplayCopyBoxScroll.Height = this.screenDisplayCopyBoxScroll.ExtentHeight;
+
+            using (InMemoryRandomAccessStream rtfStream = await this.GenerateRtf(screenCopy, fake: false))
+            {
+                this.screenDisplayCopyBox.Document.LoadFromStream(TextSetOptions.ApplyRtfDocumentDefaults | TextSetOptions.FormatRtf | TextSetOptions.Unhide, rtfStream);
+            }
+
+            this.screenDisplayCopyBox.IsReadOnly = true;
+            this.screenDisplayCopyBox.Focus(FocusState.Programmatic);
+            this.BottomAppBar.IsOpen = false;
+            this.TopAppBar.IsOpen = false;
+        }
+
+        private async Task<InMemoryRandomAccessStream> GenerateRtf(IRenderableScreenCopy screenCopy, bool fake)
+        {
+            var rtfStream = new InMemoryRandomAccessStream();
+            using (DataWriter rtf = new DataWriter(rtfStream))
+            {
+                rtf.WriteString(@"{");
+                rtf.WriteString(@"\rtf1");
+                rtf.WriteString(@"\ansi");
+                rtf.WriteString(@"\ansicpg1252");
+                rtf.WriteString(@"{\fonttbl{\f0\fmodern Consolas;}}");
+                rtf.WriteString(Environment.NewLine);
+                if (!fake)
+                {
+                    rtf.WriteString(@"{\colortbl ;");
+                    var colorTable = this.screenDisplay.ColorTheme.ColorTable;
+                    for (ScreenColor screenColor = ScreenColor.DefaultBackground; screenColor <= ScreenColor.WhiteBright; screenColor++)
+                    {
+                        var color = colorTable[screenColor];
+                        rtf.WriteString(@"\red" + color.R + @"\green" + color.G + @"\blue" + color.B + ";");
+                    }
+                    rtf.WriteString(@"}");
+                    rtf.WriteString(Environment.NewLine);
+                }
+                rtf.WriteString(@"\pard\ltrpar\f0\fs17");
+
+                StringBuilder formatCodes = new StringBuilder();
+                string fakeLineText = fake && screenCopy.Cells.Length > 0 ? new string('X', screenCopy.Cells[0].Length) : null;
+                for (int y = 0; y < screenCopy.Cells.Length; y++)
+                {
+                    if (fake)
+                    {
+                        rtf.WriteString(fakeLineText);
+                    }
+                    else
+                    {
+                        var line = screenCopy.Cells[y];
+
+                        for (int x = 0; x < line.Length; x++)
+                        {
+                            if (x == 0 || line[x - 1].BackgroundColor != line[x].BackgroundColor)
+                            {
+                                formatCodes.Append(@"\chshdng0\chcbpat" + (line[x].BackgroundColor - ScreenColor.DefaultBackground + 1));
+                                formatCodes.Append(@"\highlight" + (line[x].BackgroundColor - ScreenColor.DefaultBackground + 1));
+                                formatCodes.Append(@"\cb" + (line[x].BackgroundColor - ScreenColor.DefaultBackground + 1));
+                            }
+
+                            if (x == 0 || line[x - 1].ForegroundColor != line[x].ForegroundColor)
+                            {
+                                formatCodes.Append(@"\cf" + (line[x].ForegroundColor - ScreenColor.DefaultBackground + 1));
+                            }
+
+                            if (x == 0 || line[x - 1].Modifications.HasFlag(ScreenCellModifications.Bold) != line[x].Modifications.HasFlag(ScreenCellModifications.Bold))
+                            {
+                                formatCodes.Append(@"\b");
+                                if (!line[x].Modifications.HasFlag(ScreenCellModifications.Bold))
+                                {
+                                    formatCodes.Append("0");
+                                }
+                            }
+
+                            if (x == 0 || line[x - 1].Modifications.HasFlag(ScreenCellModifications.Underline) != line[x].Modifications.HasFlag(ScreenCellModifications.Underline))
+                            {
+                                formatCodes.Append(@"\ul");
+                                if (!line[x].Modifications.HasFlag(ScreenCellModifications.Underline))
+                                {
+                                    formatCodes.Append("0");
+                                }
+                            }
+
+                            if (formatCodes.Length > 0)
+                            {
+                                rtf.WriteString(formatCodes.ToString());
+                                formatCodes.Clear();
+                                rtf.WriteString(" ");
+                            }
+
+                            rtf.WriteString(line[x].Character.ToString());
+
+                            if (x + 1 >= screenCopy.Cells.Length)
+                            {
+                                if (line[x].Modifications.HasFlag(ScreenCellModifications.Bold))
+                                {
+                                    formatCodes.Append(@"\b0");
+                                }
+                                if (line[x].Modifications.HasFlag(ScreenCellModifications.Underline))
+                                {
+                                    formatCodes.Append(@"\ul0");
+                                }
+                            }
+                        }
+
+                        if (formatCodes.Length > 0)
+                        {
+                            rtf.WriteString(formatCodes.ToString());
+                            formatCodes.Clear();
+                        }
+                    }
+
+                    if (y + 1 < screenCopy.Cells.Length)
+                    {
+                        rtf.WriteString(@"\par" + Environment.NewLine);
+                    }
+                }
+
+                rtf.WriteString(@"}");
+
+                await rtf.StoreAsync();
+                await rtf.FlushAsync();
+                rtf.DetachStream();
+            }
+
+            rtfStream.Seek(0);
+
+            return rtfStream;
+        }
+
         public void ForceRender()
         {
             if (this.screenDisplay != null)
             {
                 this.screenDisplay.ForceRender();
             }
+        }
+
+        private void screenDisplayCopyBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.screenDisplayCopyBoxLoaded.Set();
+        }
+
+        private void copyContainer_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.HideCopyMode();
         }
     }
 }
