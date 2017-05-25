65a00870470f23387141e233d42ef21070519657
Stefan Podskubka <stefan.podskubka@live.at>
2017-05-25 21:49:59 +0200

    Update assembly/package description

--- RemoteTerminal/Properties/AssemblyInfo.cs
+++ RemoteTerminal/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RemoteTerminal")]
-[assembly: AssemblyDescription("SSH and Telnet client for Windows 8 and Windows RT")]
+[assembly: AssemblyDescription("SSH and Telnet client for Windows 8 and Windows 10")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stefan Podskubka")]
 [assembly: AssemblyProduct("RemoteTerminal")]
